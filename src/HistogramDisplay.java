3b1e7e9674878cac6931c2253839ceec8d00ff9f
Aitor Ventura <43577217+Aitor-Ventura@users.noreply.github.com>
2020-11-19 17:22:13 +0000

    Add files via upload

---
+++ src/HistogramDisplay.java
@@ -0,0 +1,46 @@
+import java.awt.Dimension;
+import java.util.Iterator;
+import java.util.Set;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame {
+    public Histogram<String> histogram;
+    
+    public HistogramDisplay (Histogram<String> histogram) {
+        super("HISTOGRAMA");
+        this.histogram = histogram;
+        setContentPane(createPanel());
+        pack();     
+    }
+    public void execute() {
+        setVisible(true);
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartpanel = new ChartPanel(createChart(createDataSet()));
+        chartpanel.setPreferredSize(new Dimension(500,400));
+        return chartpanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
+        JFreeChart  chart = ChartFactory.createBarChart("Histograma JFreeChart", "Dominios email",
+                 "Nยบ de emails", dataSet, PlotOrientation.VERTICAL, false, false, true);
+        return chart;
+    }
+    
+    public DefaultCategoryDataset createDataSet() {
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        Set<String> set = histogram.keySet();
+        for (Iterator<String> iterator = set.iterator(); iterator.hasNext();) {
+            String next = iterator.next();
+            dataSet.addValue(histogram.get(next), "", next);
+        }
+        return dataSet;
+    }
+}
---
+++ src/Kata3.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+       Histogram<String> histogram= new Histogram<>();
+       histogram.increment("gmail.com");
+       histogram.increment("yahoo.com");
+       histogram.increment("gmail.com");
+       histogram.increment("ulpgc.es");
+       histogram.increment("hotmail.com");
+       new HistogramDisplay(histogram).execute();
+    }
+}
+
